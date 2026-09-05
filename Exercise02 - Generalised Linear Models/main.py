3371a4eb1a255b80bb9c6b7f7a1e98a041e34211
Martin Clovén <you@example.com>
2026-09-06 00:22:02 +0200

    updating main

--- Exercise02 - Generalised Linear Models/main.py
+++ Exercise02 - Generalised Linear Models/main.py
@@ -1,1 +1,19 @@
+#By: Martin Clovén , 
+import numpy as np
 
+
+def pred(x, beta_0, beta_1):
+    val = np.e**(beta_0 + beta_1 * x)
+    return val
+
+
+def main(x, y):
+    for i in range(len(x)):
+        preds[i] = pred(x, beta_0, beta_1)
+    return preds
+
+
+
+x = []
+y = []
+x_ask = []
